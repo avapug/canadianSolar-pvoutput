ef4c7b83c132bee58c389c5cc33d16439ce39600
avapug <avapug@users.noreply.github.com>
2025-08-29 18:25:27 +1000

    Update pvoutput.sh

--- pvoutput.sh
+++ pvoutput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-	python3 -u ./canadian_reads.py
-	echo "Python3 script error, sleeping 60 seconds and call it again"
+	/root/zappi-env/bin/python3 -u /root/pvoutput/pvoutput.py
+	echo "Python script error, sleeping 60 seconds and call it again"
 	sleep 60s
 done
